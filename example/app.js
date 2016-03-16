93cb71cb99f0d4540b2cec4fb2ebff68efdd764a
Christian Mård <christian.mard@haahtela.fi>
2016-03-16 10:36:07 +0200

    restored original example images

--- example/app.js
+++ example/app.js
@@ -50,18 +50,38 @@ const App = React.createClass({
 
   render() {
     const images = [
-      {
-        original: 'https://www.haahtela.fi/images/pics/rakennuttamisen_referenssit/allergiatalo_3.jpg',
-        thumbnail: 'https://www.haahtela.fi/images/pics/rakennuttamisen_referenssit/allergiatalo_3.jpg',
-        originalClass: 'featured-slide',
-        thumbnailClass: 'featured-thumb',
-        description: 'Custom class for slides & thumbnails'
-      },
-      {
-        original: 'https://www.haahtela.fi/images/pics/rakennuttamisen_referenssit/allergiatalo_3.jpg',
-        thumbnail: 'https://www.haahtela.fi/images/pics/rakennuttamisen_referenssit/allergiatalo_3.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing...'
-      }
+        {
+          original: 'http://lorempixel.com/1000/600/nature/1/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+          originalClass: 'featured-slide',
+          thumbnailClass: 'featured-thumb',
+          description: 'Custom class for slides & thumbnails'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/2/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/2/',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing...'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/3/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/4/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/4/'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/5/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/5/'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/6/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/6/'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/7/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/7/'
+        }
     ]
 
     let gallery;
