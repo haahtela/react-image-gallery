11b79c1e7ba551fe63d029a375c76eb38e1237c8
HAAHTELA\kim.westerholm <kim.westerholm@haahtela.fi>
2017-11-27 09:48:09 +0200

    add rotationClass to thumbnail image elements

--- src/ImageGallery.react.jsx
+++ src/ImageGallery.react.jsx
@@ -421,6 +421,7 @@ const ImageGallery = React.createClass({
             onClick={this._wrapClick(this.slideToIndex.bind(this, index))}>
 
             <img
+              style={rotationClass}
               src={item.thumbnail}
               alt={item.thumbnailAlt}
               onError={this._handleImageError}/>
